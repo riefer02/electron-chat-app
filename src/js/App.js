40b8edc64db6f5835b1a68c91a2e5f7f6be5ee28
riefer02 <andrew.riefenstahl@gmail.com>
2020-12-31 13:51:48 -0600

    initial commit for chat app

--- src/js/App.js
+++ src/js/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 
 export default function App() {
-//   debugger;
+  //   debugger;
   const title = "Hello World";
   const enhancedTitle = title + "  - React App!";
-  return <h1>{enhancedTitle}</h1>;
+
+  const sendNotification = () => {
+    alert("Hello World!");
+  };
+
+  return (
+    <>
+      <h1>{enhancedTitle}</h1>
+      <button onClick={sendNotification}>Send Notification</button>
+    </>
+  );
 }
